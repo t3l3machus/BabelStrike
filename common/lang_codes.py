d9df2b99361392c236e7b23335e78bfb4a5d05c6
Panagiotis Chartas <t3l3machus@protonmail.com>
2023-01-20 13:17:01 +0100

    Create lang_codes.py

---
+++ common/lang_codes.py
@@ -0,0 +1,363 @@
+
+
+language_codes = {
+
+	'ab':'Abkhazian',
+
+	'aa':'Afar',
+
+	'af':'Afrikaans',
+
+	'ak':'Akan',
+
+	'sq':'Albanian',
+
+	'am':'Amharic',
+
+	'ar':'Arabic',
+
+	'an':'Aragonese',
+
+	'hy':'Armenian',
+
+	'as':'Assamese',
+
+	'av':'Avaric',
+
+	'ae':'Avestan',
+
+	'ay':'Aymara',
+
+	'az':'Azerbaijani',
+
+	'bm':'Bambara',
+
+	'ba':'Bashkir',
+
+	'eu':'Basque',
+
+	'be':'Belarusian',
+
+	'bn':'Bengali',
+
+	'bi':'Bislama',
+
+	'bs':'Bosnian',
+
+	'br':'Breton',
+
+	'bg':'Bulgarian',
+
+	'my':'Burmese',
+
+	'ca':'Catalan',
+
+	'ch':'Chamorro',
+
+	'ce':'Chechen',
+
+	'ny':'Chichewa, Chewa, Nyanja',
+
+	'zh':'Chinese',
+
+	'cv':'Chuvash',
+
+	'kw':'Cornish',
+
+	'co':'Corsican',
+
+	'cr':'Cree',
+
+	'hr':'Croatian',
+
+	'cs':'Czech',
+
+	'da':'Danish',
+
+	'dv':'Divehi',
+
+	'nl':'Dutch, Flemish',
+
+	'dz':'Dzongkha',
+
+	'en':'English',
+
+	'eo':'Esperanto',
+
+	'et':'Estonian',
+
+	'ee':'Ewe',
+
+	'fo':'Faroese',
+
+	'fj':'Fijian',
+
+	'fi':'Finnish',
+
+	'fr':'French',
+
+	'fy':'Western Frisian',
+
+	'ff':'Fulah',
+
+	'gd':'Gaelic',
+
+	'gl':'Galician',
+
+	'lg':'Ganda',
+
+	'ka':'Georgian',
+
+	'de':'German',
+
+	'el':'Greek',
+
+	'kl':'Kalaallisut',
+
+	'gn':'Guarani',
+
+	'gu':'Gujarati',
+
+	'ht':'Haitian, Haitian Creole',
+
+	'ha':'Hausa',
+
+	'he':'Hebrew',
+
+	'hz':'Herero',
+
+	'hi':'Hindi',
+
+	'ho':'Hiri Motu',
+
+	'hu':'Hungarian',
+
+	'is':'Icelandic',
+
+	'io':'Ido',
+
+	'ig':'Igbo',
+
+	'id':'Indonesian',
+
+	'ie':'Interlingue',
+
+	'iu':'Inuktitut',
+
+	'ik':'Inupiaq',
+
+	'ga':'Irish',
+
+	'it':'Italian',
+
+	'ja':'Japanese',
+
+	'jv':'Javanese',
+
+	'kn':'Kannada',
+
+	'kr':'Kanuri',
+
+	'ks':'Kashmiri',
+
+	'kk':'Kazakh',
+
+	'km':'Central Khmer',
+
+	'ki':'Kikuyu',
+
+	'rw':'Kinyarwanda',
+
+	'ky':'Kirghiz',
+
+	'kv':'Komi',
+
+	'kg':'Kongo',
+
+	'ko':'Korean',
+
+	'kj':'Kuanyama',
+
+	'ku':'Kurdish',
+
+	'lo':'Lao',
+
+	'la':'Latin',
+
+	'lv':'Latvian',
+
+	'li':'Limburgan, Limburger, Limburgish',
+
+	'ln':'Lingala',
+
+	'lt':'Lithuanian',
+
+	'lu':'Luba-Katanga',
+
+	'lb':'Luxembourgish, Letzeburgesch',
+
+	'mk':'Macedonian',
+
+	'mg':'Malagasy',
+
+	'ms':'Malay',
+
+	'ml':'Malayalam',
+
+	'mt':'Maltese',
+
+	'gv':'Manx',
+
+	'mi':'Maori',
+
+	'mr':'Marathi',
+
+	'mh':'Marshallese',
+
+	'mn':'Mongolian',
+
+	'na':'Nauru',
+
+	'nv':'Navajo, Navaho',
+
+	'nd':'North Ndebele',
+
+	'nr':'South Ndebele',
+
+	'ng':'Ndonga',
+
+	'ne':'Nepali',
+
+	'no':'Norwegian',
+
+	'ii':'Sichuan Yi',
+
+	'oc':'Occitan',
+
+	'oj':'Ojibwa',
+
+	'or':'Oriya',
+
+	'om':'Oromo',
+
+	'os':'Ossetian, Ossetic',
+
+	'pi':'Pali',
+
+	'ps':'Pashto',
+
+	'fa':'Persian',
+
+	'pl':'Polish',
+
+	'pt':'Portuguese',
+
+	'pa':'Punjabi',
+
+	'qu':'Quechua',
+
+	'ro':'Romanian',
+
+	'rm':'Romansh',
+
+	'rn':'Rundi',
+
+	'ru':'Russian',
+
+	'se':'Northern Sami',
+
+	'sm':'Samoan',
+
+	'sg':'Sango',
+
+	'sa':'Sanskrit',
+
+	'sc':'Sardinian',
+
+	'sr':'Serbian',
+
+	'sn':'Shona',
+
+	'sd':'Sindhi',
+
+	'si':'Sinhala',
+
+	'sk':'Slovak',
+
+	'sl':'Slovenian',
+
+	'so':'Somali',
+
+	'st':'Southern Sotho',
+
+	'es':'Spanish',
+
+	'su':'Sundanese',
+
+	'sw':'Swahili',
+
+	'ss':'Swati',
+
+	'sv':'Swedish',
+
+	'tl':'Tagalog',
+
+	'ty':'Tahitian',
+
+	'tg':'Tajik',
+
+	'ta':'Tamil',
+
+	'tt':'Tatar',
+
+	'te':'Telugu',
+
+	'th':'Thai',
+
+	'bo':'Tibetan',
+
+	'ti':'Tigrinya',
+
+	'to':'Tonga',
+
+	'ts':'Tsonga',
+
+	'tn':'Tswana',
+
+	'tr':'Turkish',
+
+	'tk':'Turkmen',
+
+	'tw':'Twi',
+
+	'ug':'Uighur',
+
+	'uk':'Ukrainian',
+
+	'ur':'Urdu',
+
+	'uz':'Uzbek',
+
+	've':'Venda',
+
+	'vi':'Vietnamese',
+
+	'vo':'Volapük',
+
+	'wa':'Walloon',
+
+	'cy':'Welsh',
+
+	'wo':'Wolof',
+
+	'xh':'Xhosa',
+
+	'yi':'Yiddish',
+
+	'yo':'Yoruba',
+
+	'za':'Zhuang',
+
+	'zu':'Zulu'
+
+}
